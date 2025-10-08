0e36c194bc077a59fc82c9367b341aed359e3f56
brunogoncalvesferreira <brunogoncalvesferreira@outlook.com>
2025-10-08 09:02:39 -0300

    test: test search-featured success

---
+++ src/routes/search-fetatured-properties.routes.e2e.test.ts
@@ -0,0 +1,16 @@
+import { expect, test } from 'vitest'
+import { app } from '../app'
+import request from 'supertest'
+
+test('(E2E) Search featured properties', async () => {
+  await app.ready()
+
+  const response = await request(app.server).get(
+    '/properties/search-featured-properties'
+  )
+
+  expect(response.statusCode).toEqual(200)
+  expect(response.body).toEqual({
+    properties: expect.any(Array),
+  })
+})
